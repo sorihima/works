7df2a627d048209cb0665668c766cbf8e41dc02e
dsd <48131517+sorihima@users.noreply.github.com>
2025-03-28 19:38:04 +0200

    Практична 4

--- src/homework/hw04.rs
+++ src/homework/hw04.rs
@@ -1,33 +1,30 @@
-const WIDTH: usize = 7; 
-const HEIGHT: usize = 4; 
+const SIZE: usize = 5; 
 
 fn main() {
     let mut output = String::new();
-
-    for i in 0..HEIGHT {
-        for _ in 0..(WIDTH / 2 - i) {
+  
+    for i in 0..SIZE {
+        for _ in 0..(SIZE - i - 1) {
             output.push(' ');
         }
-        output.push('/');
-        for _ in 0..(i * 2) {
-            output.push(' ');
+        for _ in 0..(2 * i + 1) {
+            output.push('*');
         }
-        output.push('\\');
         output.push('\n');
     }
 
-    output.push_str(&"-".repeat(WIDTH));
+    for _ in 0..(2 * SIZE + 1) {
+        output.push('*');
+    }
     output.push('\n');
 
-    for i in (0..HEIGHT).rev() {
-        for _ in 0..(WIDTH / 2 - i) {
+    for i in (0..SIZE).rev() {
+        for _ in 0..(SIZE - i - 1) {
             output.push(' ');
         }
-        output.push('\\');
-        for _ in 0..(i * 2) {
-            output.push(' ');
+        for _ in 0..(2 * i + 1) {
+            output.push('*');
         }
-        output.push('/');
         output.push('\n');
     }
 
