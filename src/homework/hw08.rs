17888616cb1442ea1fc9562fc4fc19a0b8874470
dsd <48131517+sorihima@users.noreply.github.com>
2025-05-02 20:22:06 +0300

    Update hw08.rs

---
+++ src/homework/hw08.rs
@@ -0,0 +1,41 @@
+fn is_prime(n: &u32) -> bool {
+    if *n < 2 {
+        return false;
+    }
+    if *n == 2 {
+        return true;
+    }
+    if n % 2 == 0 {
+        return false;
+    }
+    let sqrt_n = (*n as f64).sqrt() as u32;
+    for i in (3..=sqrt_n).step_by(2) {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_prime() {
+        let test_data = [
+            (0, false),
+            (1, false),
+            (2, true),
+            (3, true),
+            (4, false),
+            (5, true),
+            (100, false),
+            (10007, true),
+        ];
+
+        test_data.iter().for_each(|(n, prime)| {
+            assert_eq!(is_prime(n), *prime)
+        });
+    }
+}
